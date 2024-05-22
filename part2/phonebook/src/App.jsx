da1fda6cf278dccf2f393fa1894bb944fff21307
Jason Ling <sanicthefastmaster@gmail.com>
2024-05-22 16:16:26 +0800

    Added Exercise 2.6 - 2.09 Phonebook, Exercise 2.10 - refactoring to components is not completed yet

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-1234567', id: 1},
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+    const [newName, setNewName] = useState('')
+    const [newNum, setNewNum] = useState('')
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const resetNewPerson = () => {
+        setNewName('') //reset value of new name to erase duplicate name
+        setNewNum('') //reset value of new number to erase duplicate number
+    }
+    const addName = (event) => {
+
+        event.preventDefault()
+        if (persons.some(person => person.name === newName)) {
+            alert(`${newName} is already added to the phonebook`)
+            // don't concat the new person and instead generate alert of duplicate
+            resetNewPerson() // reset the new person
+        }
+        else {
+            const nameObject = {
+                name: newName,
+                number: newNum,
+                id: persons.length + 1,
+            }
+
+            setPersons(persons.concat(nameObject)) //set value of new name to be added to Person array
+            resetNewPerson() //erase the new person
+        } //concat the new name to person and reset value of new name state
+
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value) //set new name to the input given
+    }
+
+    const handleNumChange = (event) => {
+        setNewNum(event.target.value) //set new num to the input given
+    }
+
+    const filteredResults = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    const filterName = (event) => {
+        setSearchTerm(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <div>
+                filter: <input onChange={filterName}/>
+            </div>
+            <h2>Add new contact</h2>
+            <form onSubmit={addName}>
+                <div> name: <input value={newName} onChange={handleNameChange}/> </div>
+                <div> number: <input value={newNum} onChange={handleNumChange} /> </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {
+                    filteredResults.map(
+                        person =>
+                            <p key={person.name}>{person.name} {person.number}</p>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default App
